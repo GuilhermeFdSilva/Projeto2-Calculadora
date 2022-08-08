b5555ad706b790e01cc8fdae3c70d6ec4d15798a
GuilhermeFdSilva <francaguilherme27@gmail.com>
2022-08-08 19:58:23 -0300

    Projeto Calculadora

---
+++ calculadora/src/br/com/cod3r/calc/visao/Display.java
@@ -0,0 +1,21 @@
+package br.com.cod3r.calc.visao;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel {
+	private final JLabel label;
+	public Display() {
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("1234,56");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 40));
+		setLayout(new FlowLayout(FlowLayout.RIGHT));
+		add(label);
+	}
+}
